40b7156c44af3efb5822ddefeac53d28b751499d
DUCTHINHPM14014 <thinhseven4@gmail.com>
2019-10-08 17:42:20 +0700

    Exercise5a

--- Exercise5/Exercise5a/Program.cs
+++ Exercise5/Exercise5a/Program.cs
@@ -10,6 +10,52 @@ namespace Exercise5a
     {
         static void Main(string[] args)
         {
+           Print_a_2_z_v4();
+           Print_a_2_z_v5();
+        Console.ReadKey().KeyChar;
+             while (true)
+             { 
+                 char c = Console.ReadKey().KeyChar;
+                  Console.WriteLine(Char2Code)
+             }
+
         }
-    }
-}
+         public static void Print_a_2_z_v4()
+        {
+           for (char c ='a';c <= 'z';c++)
+            {
+               
+                                  Console.WriteLine(c);
+             }
+        }
+         public static void Print_a_2_z_v5()
+         {
+             string s ="abcdefghijklmnopqrstuvwxyz";
+             for (int i = 0; i < s.Length; i++)
+                 Console.WriteLine(s[i]);
+         } 
+                 public static int Char2Code(char c)
+                      (  
+                           return (int)c;
+                )
+        public static bool IsUpper (char c)
+        { 
+            if 
+             return false;
+             
+        }
+         public static bool IsLower (char c)
+         { 
+              return false ;
+         }
+              
+         
+    
+              public static bool ToUpper (char c)
+{
+         return c ;
+              
+      }
+   }
+}  
+
